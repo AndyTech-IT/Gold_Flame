8eb69f5e9e33df9ccffe2f6d38fb6bbde5032add
Андрей Коковихин <a.kokovikhin@mail.ru>
2022-11-14 10:43:21 +0800

    Refactor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Add_Player = new System.Windows.Forms.Button();
+            this.TouglleServer = new System.Windows.Forms.Button();
+            this.ServerTitle_TB = new System.Windows.Forms.TextBox();
+            this.Refresh_Servers = new System.Windows.Forms.Button();
+            this.Servers_LV = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // Add_Player
+            // 
+            this.Add_Player.Location = new System.Drawing.Point(627, 415);
+            this.Add_Player.Name = "Add_Player";
+            this.Add_Player.Size = new System.Drawing.Size(161, 23);
+            this.Add_Player.TabIndex = 0;
+            this.Add_Player.Text = "Новый игрок";
+            this.Add_Player.UseVisualStyleBackColor = true;
+            this.Add_Player.Click += new System.EventHandler(this.Add_Player_Click);
+            // 
+            // TouglleServer
+            // 
+            this.TouglleServer.Location = new System.Drawing.Point(627, 386);
+            this.TouglleServer.Name = "TouglleServer";
+            this.TouglleServer.Size = new System.Drawing.Size(161, 23);
+            this.TouglleServer.TabIndex = 1;
+            this.TouglleServer.Text = "Включить сервер";
+            this.TouglleServer.UseVisualStyleBackColor = true;
+            this.TouglleServer.Click += new System.EventHandler(this.TouglleServer_Click);
+            // 
+            // ServerTitle_TB
+            // 
+            this.ServerTitle_TB.Location = new System.Drawing.Point(627, 357);
+            this.ServerTitle_TB.Name = "ServerTitle_TB";
+            this.ServerTitle_TB.Size = new System.Drawing.Size(161, 23);
+            this.ServerTitle_TB.TabIndex = 2;
+            this.ServerTitle_TB.Text = "Server";
+            this.ServerTitle_TB.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ServerTitle_TB_KeyUp);
+            this.ServerTitle_TB.Leave += new System.EventHandler(this.ServerTitle_TB_Leave);
+            // 
+            // Refresh_Servers
+            // 
+            this.Refresh_Servers.Location = new System.Drawing.Point(713, 12);
+            this.Refresh_Servers.Name = "Refresh_Servers";
+            this.Refresh_Servers.Size = new System.Drawing.Size(75, 23);
+            this.Refresh_Servers.TabIndex = 3;
+            this.Refresh_Servers.Text = "Refresh";
+            this.Refresh_Servers.UseVisualStyleBackColor = true;
+            this.Refresh_Servers.Click += new System.EventHandler(this.Refresh_Servers_Click);
+            // 
+            // Servers_LV
+            // 
+            this.Servers_LV.FullRowSelect = true;
+            this.Servers_LV.GridLines = true;
+            this.Servers_LV.Location = new System.Drawing.Point(519, 41);
+            this.Servers_LV.Name = "Servers_LV";
+            this.Servers_LV.Size = new System.Drawing.Size(269, 310);
+            this.Servers_LV.TabIndex = 4;
+            this.Servers_LV.UseCompatibleStateImageBehavior = false;
+            this.Servers_LV.View = System.Windows.Forms.View.Details;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Servers_LV);
+            this.Controls.Add(this.Refresh_Servers);
+            this.Controls.Add(this.ServerTitle_TB);
+            this.Controls.Add(this.TouglleServer);
+            this.Controls.Add(this.Add_Player);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button Add_Player;
+        private Button TouglleServer;
+        private TextBox ServerTitle_TB;
+        private Button Refresh_Servers;
+        private ListView Servers_LV;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,135 +2,89 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Security.AccessControl;
 using System.Text;
-using UDP_Server;
+using NetworkGame_Asset;
+using static Gold_Flame.GameServer;
 
 namespace Gold_Flame
 {
     public partial class Form1 : Form
     {
-        Dictionary<string, IPEndPoint> _senders;
-
+        GameClient user = new();
         public Form1()
         {
             InitializeComponent();
-            _senders = new Dictionary<string, IPEndPoint>();
-        }
-
-        private static byte[] Encode_Hello_Message(string user_name, ushort port) => Encode_String(user_name).Concat(Encode_Ushort(port)).ToArray();
-
-        private static byte[] Encode_String(string value)
-        {
-            byte[] str_bytes = Encoding.UTF8.GetBytes(value);
-            return Encode_Ushort((ushort)str_bytes.Length).Concat(str_bytes).ToArray();
-        }
-
-        private static byte[] Encode_Ushort(ushort value)
-        {
-            return BitConverter.GetBytes(value);
+            GameServer.Client_Response += On_Client_Response;
+            user.Server_Response += On_Server_Response;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            UDP_Listener.Start(80);
-            UDP_Listener.Data_Received += On_Received;
-
-
-            TCP_Listener client1_listener = new();
-            client1_listener.Data_Received += On_1_Recieved;
-            ushort port1 = client1_listener.Start();
-
-            UDP_Sender.SendBroadcast(Encode_Hello_Message("User1", port1), 80);
-
-            TCP_Listener client2_listener = new();
-            client2_listener.Data_Received += On_2_Recieved;
-            ushort port2 = client2_listener.Start();
-
-            UDP_Sender.SendBroadcast(Encode_Hello_Message("User2", port2), 80);
+            Refresh_Servers_Click(sender, e);
         }
 
-        void On_1_Recieved(IPEndPoint? sender, byte[] data)
+        void On_Client_Response((int message_type, byte[] data) response)
         {
-            Debug.WriteLine($"1 Recieved: {Encoding.UTF8.GetString(data)} from {sender}");
+            MessageBox.Show(response.data.Decode_String());
         }
 
-        void On_2_Recieved(IPEndPoint? sender, byte[] data)
+        void On_Server_Response(GameMessage response)
         {
-            Debug.WriteLine($"2 Recieved: {Encoding.UTF8.GetString(data)} from {sender}");
+            GameServer.Info info = response.Data.ToArray().Decode_GameServer_Info();
+            Invoke(Add_Server, new object[] { response.Hostname, info.Players_Count, info.Max_Players_Count });
         }
 
-
-
-        int PORT_NO = 5000;
-        const string SERVER_IP = "127.0.0.1";
-        private void Server(object? data)
+        void Add_Server(string name, int players, int max)
         {
-            string name = data?.ToString() ?? "No name";
-            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
-            TcpListener listener = new TcpListener(localAdd, PORT_NO++);
-            Debug.WriteLine("Listening...");
-            listener.Start();
-
-            //---incoming client connected---
-            TcpClient client = listener.AcceptTcpClient();
-
-            //---get the incoming data through a network stream---
-            NetworkStream nwStream = client.GetStream();
-            byte[] buffer = new byte[client.ReceiveBufferSize];
-
-            //---read incoming stream---
-            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
-
-            //---convert the data received into a string---
-            string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            Debug.WriteLine("Received : " + dataReceived);
-
-            //---write back the text to the client---
-            Debug.WriteLine("Sending back : " + name);
-            nwStream.Write(Encoding.UTF8.GetBytes(name));
-            client.Close();
-            listener.Stop();
+            var item = new ListViewItem(new[] { name, players.ToString(), max.ToString() });
+            Servers_LV.Items.Add(item);
         }
 
-        private void On_Received(byte[] data, IPAddress? ip)
+        private void Add_Player_Click(object sender, EventArgs e)
         {
-            byte[] len_data = data.Take(2).ToArray();
-            data = data.Skip(2).ToArray();
-            ushort name_len = BitConverter.ToUInt16(len_data);
-            string sender_name = Encoding.UTF8.GetString(data.Take(name_len).ToArray());
-            data = data.Skip(name_len).ToArray();
-            ushort port = BitConverter.ToUInt16(data.Take(2).ToArray());
+            new Form1().Show();
+        }
 
-            string textToSend = "";
-            IPEndPoint sender = new(ip!, port);
-            if (_senders.ContainsKey(sender_name) == false)
+        private void TouglleServer_Click(object sender, EventArgs e)
+        {
+            if (GameServer.Enabled)
             {
-                _senders.Add(sender_name, sender);
-                textToSend = $"Welcome {sender_name}!";
+                TouglleServer.Text = "Включить сервер";
+                GameServer.Stop();
             }
             else
             {
-                textToSend = $"Hello again {sender_name}..";
+                TouglleServer.Text = "Выключить сервер";
+                GameServer.Start();
             }
+        }
 
+        private void ServerTitle_TB_Leave(object sender, EventArgs e)
+        {
+            ServerTitle_TB.Text = GameServer.Server_Title;
+        }
 
-            //---create a TCPClient object at the IP and port no.---
-            TcpClient client = new();
+        private void ServerTitle_TB_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    GameServer.Server_Title = ServerTitle_TB.Text;
+                    break;
+                case Keys.Escape:
+                    ServerTitle_TB.Text = GameServer.Server_Title;
+                    break;
+            }
+        }
 
-            //---send the text---
-            Debug.WriteLine("Sending : " + textToSend);
-            byte[] bytesToSend = Encoding.UTF8.GetBytes(textToSend);
-            client.Connect(sender);
-            client.Client.Send(bytesToSend);
-
-            ////---read back the text---
-            //byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-            //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-            //Debug.WriteLine("Received : " + Encoding.UTF8.GetString(bytesToRead, 0, bytesRead));
-            client.Close();
+        private void Refresh_Servers_Click(object sender, EventArgs e)
+        {
+            Servers_LV.Clear();
+            Servers_LV.Columns.Add("Название", 120);
+            Servers_LV.Columns.Add("Онлайн", 70);
+            Servers_LV.Columns.Add("Максимум", 70);
+            user.SearchServers();
         }
     }
 }
